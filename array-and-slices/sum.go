8dd50d5cf63a3f35638d844db791f325e2ead7f4
tesla59 <nishant@heim.id>
2024-01-28 18:21:40 +0530

    arrayandslices: add SumAllTails

--- array-and-slices/sum.go
+++ array-and-slices/sum.go
@@ -15,3 +15,15 @@ func SumAll(slices ...[]int) []int {
 	}
 	return answer
 }
+
+func SumAllTails(slices ...[]int) []int {
+	answer := []int{}
+	for _, v := range slices {
+		if len(v) == 0 {
+			answer = append(answer, 0)
+		} else {
+			answer = append(answer, Sum(v[1:]))
+		}
+	}
+	return answer
+}
--- array-and-slices/sum_test.go
+++ array-and-slices/sum_test.go
@@ -23,3 +23,23 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := arrayandslices.SumAllTails([]int{1, 2}, []int{4, 7, 9})
+		want := []int{2, 16}
+
+		if slices.Equal(got, want) != true {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := arrayandslices.SumAllTails([]int{}, []int{4, 7, 9})
+		want := []int{0, 16}
+
+		if slices.Equal(got, want) != true {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
