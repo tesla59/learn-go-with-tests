e26ca9fd95d9afa3f2409b8be51c57e726eabb55
tesla59 <nishant@heim.id>
2024-01-28 18:46:54 +0530

    struct: refactor tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -6,31 +6,33 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	rectangle := structs.Rectangle{Width: 10.0, Height: 10.0}
-	got := rectangle.Perimeter()
-	want := 40.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	checkPerimeter := []struct {
+		shape structs.Shape
+		want  float64
+	}{
+		{structs.Rectangle{Width: 10, Height: 10}, 40.0},
+		{structs.Circle{Radius: 10}, 62.83185307179586},
+	}
+	for _, v := range checkPerimeter {
+		got := v.shape.Perimeter()
+		if got != v.want {
+			t.Errorf("got %g want %g", got, v.want)
+		}
 	}
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape structs.Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+	checkArea := []struct {
+		shape structs.Shape
+		want  float64
+	}{
+		{structs.Rectangle{Width: 12, Height: 6}, 72.0},
+		{structs.Circle{Radius: 10}, 314.1592653589793},
+	}
+	for _, v := range checkArea {
+		got := v.shape.Area()
+		if got != v.want {
+			t.Errorf("got %g want %g", got, v.want)
 		}
 	}
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := structs.Rectangle{Width: 12.0, Height: 6.0}
-		want := 72.0
-		checkArea(t, rectangle, want)
-	})
-	t.Run("circle", func(t *testing.T) {
-		circle := structs.Circle{Radius: 10}
-		want := 314.1592653589793
-		checkArea(t, circle, want)
-	})
 }
