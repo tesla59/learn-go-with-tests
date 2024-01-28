f447711a5532dfbb05ee9dd135f3d050f72694fd
tesla59 <nishant@heim.id>
2024-01-28 16:18:47 +0530

    hello_test.go: add assertCorrectMessage helper

--- hello_test.go
+++ hello_test.go
@@ -7,24 +7,27 @@ func TestHello(t *testing.T) {
 		got := Hello("Mike")
 		want := "Hello, Mike"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("saying hello to a place", func(t *testing.T) {
 		got := Hello("Paris")
 		want := "Hello, Paris"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("saying hello to an empty string", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	// t.Helper() is needed to tell the test suite that this method is a helper.
+	// By doing this when it fails the line number reported will be in our function call rather than inside our test helper.
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
